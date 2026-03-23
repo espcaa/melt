345c55d65f61e258396762161a98c23194fb0a09
alice <122629939+espcaa@users.noreply.github.com>
2026-03-23 22:07:15 +0100

    minor fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://smelt.hackclub.com",
   output: "server",
   adapter: node({
     mode: "standalone",
