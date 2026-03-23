ee60fe098d6260498d75bc635d83e16b6a5b0033
alice <122629939+espcaa@users.noreply.github.com>
2026-03-23 22:14:14 +0100

    get env vars at runtime

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client/web";
 
 export const turso = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL,
-  authToken: import.meta.env.TURSO_AUTH_TOKEN,
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
